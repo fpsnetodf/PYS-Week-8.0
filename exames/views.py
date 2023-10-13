9fc76337fe282b0970c5b085407d7f605de54bfa
fpsnetodf <fps_netodf@hotmail.com>
2023-10-12 21:16:02 -0300

    bootstrap-5

--- exames/views.py
+++ exames/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .models import TiposExames
+from .models import TiposExames, PedidosExames, SolicitacaoExame
 from django.http import HttpResponse
+from datetime import datetime
+from django.contrib.messages import constants
+from django.contrib import messages
 
 
 
@@ -9,6 +12,7 @@ from django.http import HttpResponse
 @login_required
 def solicitar_exames(request):
     tipos_exames = TiposExames.objects.all()
+    
     if request.method == "GET":
         return render(request, 'solicitar_exames.html', {'tipos_exames': tipos_exames})
     elif request.method == "POST":
@@ -21,7 +25,56 @@ def solicitar_exames(request):
                 preco += exame.preco
         
         exames = request.POST.getlist('exames')
-        
+
 
         return render (request, "solicitar_exames.html", {"tipos_exames":tipos_exames, "solicitacao_exames" : solicitacao_exames, "preco":preco})
+
+def fechar_pedido(request):
+    if request.method == "GET":
+        return HttpResponse("estou GET do pedidos_exames")
+    if request.method == "POST":
+        exames_id = request.POST.getlist('exames')
+        solicitacao_exames = TiposExames.objects.filter(id__in=exames_id)
+        pedidos_exames = PedidosExames(
+            usuario = request.user,
+            data = datetime.now()
+        )  
+        pedidos_exames.save()  
+
+        for exame in solicitacao_exames:
+            solicitacao_exames_temp = SolicitacaoExame(
+                usuario = request.user, 
+                exame = exame, 
+                status = 'E'
+            )
+            solicitacao_exames_temp.save()
+            pedidos_exames.exames.add(solicitacao_exames_temp)
+
+        pedidos_exames.save()
+        messages.add_message(request, constants.SUCCESS, "Pedidos Salvos com sucesso")
+
+        return redirect("gerenciar_pedidos")
     
+
+@login_required
+def gerenciar_pedidos(request):
+    pedidos_exames = PedidosExames.objects.filter(usuario=request.user)
+    return render(request, 'gerenciar_pedidos.html', {'pedidos_exames': pedidos_exames})
+
+
+
+@login_required
+def cancelar_pedido(request, pedido_id):
+    pedido = PedidosExames.objects.get(id=pedido_id)
+    if not pedido.usuario == request.user:
+        messages.add_message(request, constants.ERROR, 'Esse pedido não é seu')
+        return redirect('gerenciar_pedidos')
+    
+    if not pedido.usuario == request.user:
+        messages.add_message(request, constants.ERROR, 'Esse pedido não é seu')
+        return redirect('gerenciar_pedidos')
+    
+    pedido.agendado = False
+    pedido.save()
+    messages.add_message(request, constants.SUCCESS, 'Pedido excluido com sucesso')
+    return redirect('gerenciar_pedidos')
